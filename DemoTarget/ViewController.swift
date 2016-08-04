a127795b5429d874f0c1affbe668040fef4d7cbd
Rajdeep Arora <rajdaror@in.ibm.com>
2016-08-04 12:05:16 +0530

    print added

--- DemoTarget/ViewController.swift
+++ DemoTarget/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        print("heelo")
         for family: String in UIFont.familyNames()
         {
             print("\(family)")
